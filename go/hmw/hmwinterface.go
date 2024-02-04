544adc1a278b9d43822be65d8bee95d1c99e765a
muh1ddinn <rmamuhit@gmail.com>
2024-02-04 12:28:34 +0500

    ninee

--- go/hmw/hmwinterface.go
+++ go/hmw/hmwinterface.go
@@ -1,7 +1,6 @@
 package main
 
-import "fmt"
-
+/*
 type Worker interface {
 	work() string
 }
@@ -31,3 +30,61 @@ func main() {
 	Printwork(t)
 
 }
+
+type shape interface {
+	area() float64
+}
+
+func Prinareaa(e shape) {
+
+	fmt.Println("area of area : ", e.area())
+
+}
+
+type uburchak struct {
+	height float64
+	base   float64
+}
+
+func (u uburchak) area() float64 {
+
+	return float64(1) / float64(2) * u.height * u.base
+}
+
+type torburch struct {
+	height float64
+	basee  float64
+}
+
+func (p torburch) area() float64 {
+
+	return p.height * p.basee
+}
+
+func main() {
+
+	uch := uburchak{
+		height: 7445.4,
+		base:   392.4,
+	}
+
+	area := uch.area()
+	fmt.Printf("area this %5f\n", area)
+
+	to := torburch{
+		height: 45.3,
+		basee:  54.3,
+	}
+
+	fmt.Println("torburchak : ", to.area())
+
+	uuch := uburchak{
+		height: 55,
+		base:   54.4,
+	}
+
+	Prinareaa(&uuch)
+	Prinareaa(to)
+
+}
+*/
